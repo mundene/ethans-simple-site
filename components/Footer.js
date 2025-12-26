65f21b9602211ecaddb831d7935a7279b0135dfd
mundene <mundene35@gmail.com>
2025-12-25 19:04:17 -0500

    Update Footer.js
    Changed link url in Footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <a href="https://www.netlify.com" target="_blank" rel="noopener">
+        <a href="https://www.linkedin.com/in/ethan-munden" target="_blank" rel="noopener">
           <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
         </a>
       </footer>
