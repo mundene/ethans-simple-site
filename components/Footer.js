8529f6ea0b71ee8c23410dcd2e54643d72dd2d53
mundene <mundene35@gmail.com>
2025-12-25 23:15:41 -0500

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         <a href="https://www.linkedin.com/in/ethan-munden" target="_blank" rel="noopener">
-          <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+          <img src="/smiley.svg" alt="Netlify Logo" className={styles.logo} />
         </a>
       </footer>
     </>
